4d07d2b601d907556c70d5b740c699b5e7ce7143
ananya.roy@databricks.com <ananya.roy@databricks.com>
2026-02-11 02:38:29 +0000

    remove all taxi data reference from validation script
    remove all taxi data reference from validation script

--- boq_mlops_project/training/notebooks/TrainWithFeatureStore.py
+++ boq_mlops_project/training/notebooks/TrainWithFeatureStore.py
@@ -27,6 +27,11 @@
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC
+
+# COMMAND ----------
+
 import os
 notebook_path =  '/Workspace/' + os.path.dirname(dbutils.notebook.entry_point.getDbutils().notebook().getContext().notebookPath().get())
 %cd $notebook_path
